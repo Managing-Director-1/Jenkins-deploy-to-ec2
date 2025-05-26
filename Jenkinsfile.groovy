d2a39dc5b92060ea5267cfced44fb19192bd05d7
Managing-Director-1 <154487584+Managing-Director-1@users.noreply.github.com>
2025-05-26 10:41:01 -0700

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -3,8 +3,8 @@ pipeline {
 
     parameters {
         string(name: 'EC2_IP', defaultValue: '172.31.34.253', description: 'EC2 Instance IP Address')
-        string(name: 'GIT_BRANCH', defaultValue: 'jenkins', description: 'Git Branch to checkout')
-        string(name: 'GIT_URL', defaultValue: 'https://github.com/seunayolu/jenkins_deploy_ec2.git', description: 'Git Repository URL')
+        string(name: 'GIT_BRANCH', defaultValue: 'Jenkins', description: 'Git Branch to checkout')
+        string(name: 'GIT_URL', defaultValue: 'https://github.com/Managing-Director-1/Jenkins-deploy-to-ec2.git', description: 'Git Repository URL')
         string(name: 'SSH_CREDENTIALS', defaultValue: 'ec2', description: 'SSH Credentials ID')
         string(name: 'REMOTE_USER', defaultValue: 'ubuntu', description: 'Remote SSH User')
     }
